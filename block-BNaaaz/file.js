23c714ad1cbc7f000a035a7a97097dc9b0cd511e
Ravindra-Shastri <ravindrashastri90@gmail.com>
2021-10-26 14:20:45 +0530

    index.md

--- block-BNaaaz/file.js
+++ block-BNaaaz/file.js
@@ -1,10 +1,14 @@
 var http = require('http');
 
+var fs = require('fs');
+
 var server = http.createServer(handleRequest);
 
-
-
+function handleRequest(req, res){
+res.setHeader('content-type', 'text/plain');
+fs.createReadStream('./readme.txt').pipe(res);
+}
 
 server.listen(3000, () => {
-    console.log('server is listening on PORT 3000')
+    console.log('server listening on port 3k');
 })
