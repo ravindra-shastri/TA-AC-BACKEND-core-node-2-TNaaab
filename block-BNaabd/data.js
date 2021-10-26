43062e1fe4ea1dc66d143cb40c23f43222f62373
Ravindra-Shastri <ravindrashastri90@gmail.com>
2021-10-26 15:49:14 +0530

    index.md

--- block-BNaabd/data.js
+++ block-BNaabd/data.js
@@ -1,11 +1,28 @@
 var http = require('http');
 
+var qs = require('querystring');
+
 var server = http.createServer(handleRequest);
 
 function handleRequest(req, res){
-
+var store = '';
+req.on('data', (chunk) => {
+    store += chunk;
+})
+req.on('end', () => {
+    if(req.method === 'POST' && req.url === '/json'){
+        console.log(store);
+        res.detHeader('content-type',"application/json")
+        res.end(store);
+    }
+    if(req.method === 'POST' && req.url === '/form'){
+        console.log(store);
+        var formData = qs.parse(store);
+        res.end(JSON.stringify(formData));
+    }
+})
 }
 
 server.listen(7000, () => {
-    console.log('server listening on 7k')
+    console.log('server listening on port 7k');
 })
